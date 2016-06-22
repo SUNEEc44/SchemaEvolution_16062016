8b7140412c22217a653358f82edb68aa49e0e244
naresh9989 <naresh.oct29@gmail.com>
2016-06-22 10:39:03 +0530

    added propertyReader

---
+++ SchemaEvolution_Batch16062016/src/main/java/com/inetsolv/PropertyReader.java
@@ -0,0 +1,31 @@
+package com.inetsolv;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class PropertyReader{
+	public static Properties readProperty(String[] args) {
+		try {
+			File file = new File("D:/project/naresh.txt");
+			FileInputStream fileInput = new FileInputStream(file);
+			Properties properties = new Properties();
+			properties.load(fileInput);
+			fileInput.close();
+
+			Enumeration enuKeys = properties.keys();
+			while (enuKeys.hasMoreElements()) {
+				String key = (String) enuKeys.nextElement();
+				String value = properties.getProperty(key);
+				System.out.println(key + ": " + value);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
